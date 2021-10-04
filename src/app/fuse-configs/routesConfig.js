45c7c7a249637f4880e41e8e2d27e86bb8ed1a86
Matsushita Shota <ms@MatsushitanoMacBook-Pro.local>
2021-10-05 08:50:39 +0900

    ログイン認証設定

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -3,31 +3,32 @@ import FuseUtils from '@fuse/utils';
 import ExampleConfig from 'app/main/example/ExampleConfig';
 import FuseLoading from '@fuse/core/FuseLoading';
 import Error404Page from 'app/main/404/Error404Page';
+import LoginConfig from '../auth/login/LoginConfig';
 
-const routeConfigs = [ExampleConfig];
+const routeConfigs = [LoginConfig, ExampleConfig];
 
 const routes = [
-  // if you want to make whole app auth protected by default change defaultAuth for example:
-  // ...FuseUtils.generateRoutesFromConfigs(routeConfigs, ['admin','staff','user']),
-  // The individual route configs which has auth option won't be overridden.
-  ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
-  {
-    exact: true,
-    path: '/',
-    component: () => <Redirect to="/example" />,
-  },
-  {
-    path: '/loading',
-    exact: true,
-    component: () => <FuseLoading />,
-  },
-  {
-    path: '/404',
-    component: () => <Error404Page />,
-  },
-  {
-    component: () => <Redirect to="/404" />,
-  },
+    // if you want to make whole app auth protected by default change defaultAuth for example:
+    // ...FuseUtils.generateRoutesFromConfigs(routeConfigs, ['admin','staff','user']),
+    // The individual route configs which has auth option won't be overridden.
+    ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
+    {
+        exact: true,
+        path: '/',
+        component: () => <Redirect to="/example" />,
+    },
+    {
+        path: '/loading',
+        exact: true,
+        component: () => <FuseLoading />,
+    },
+    {
+        path: '/404',
+        component: () => <Error404Page />,
+    },
+    {
+        component: () => <Redirect to="/404" />,
+    },
 ];
 
 export default routes;
